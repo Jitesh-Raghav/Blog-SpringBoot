776378c4296f68333445628bff76cb23b40ff71d
Jitesh Kumar Raghav <jkr18042001@gmail.com>
2024-01-20 18:40:43 +0530

    jwt

---
+++ src/main/java/com/example/springbootblog/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,22 @@
+package com.example.springbootblog.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    //This class throws exception when a unauthorized user is trying to access the app
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+}
